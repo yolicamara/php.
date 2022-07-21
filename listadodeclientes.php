db87dd523a1db41a923705eab731e24801394562
ycamara <yolicamara2@hotmail.com>
2022-07-21 14:14:36 -0300

    cambios

---
+++ listadodeclientes.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if($_POST){
+    $nombre = $_POST["txtNombre"];
+    $dni = $_POST["txtdni"];
+    $telefono = 
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de clientes</title>
+    <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">"
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Listado de clientes</h1>
+            </div>
+            <div class="row">
+                <div class="col-6">
+
+                </div>
+            </div>
+        </div>
